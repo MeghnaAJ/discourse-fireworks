7250a027a611a33571c6ea107643d4c096b5fd20
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-02-22 15:31:53 +0800

    Update and rename head_tag.html to head_tag.js

--- desktop/head_tag.js
+++ desktop/head_tag.js
@@ -4,11 +4,26 @@
     const tsParticlesCdn = document.createElement("script");
     tsParticlesCdn.src = "https://cdn.jsdelivr.net/npm/@tsparticles/fireworks@3.7.1/tsparticles.fireworks.bundle.min.js";
     document.head.appendChild(tsParticlesCdn);
-    tsParticlesCdn.onload = function() {
-      (async () => {
-        await fireworks();
-      })();
+  
+    if (settings.tsparticles_sound_enabled == "true") {
+      tsParticlesCdn.onload = function() {
+        (async () => {
+          await fireworks({
+            rate: parseInt(settings.tsParticles_fireworks_rate);
+          });
+        })();
+      }
+    } else {
+      tsParticlesCdn.onload = function() {
+        (async () => {
+          await fireworks({
+            rate: parseInt(settings.tsParticles_fireworks_rate),
+            sounds: false,
+          });
+        })();
+      }
     }
+  
     document.addEventListener("DOMContentLoaded", function() {
       // Select the fireworks div
       const fireworksDiv = document.getElementById('fireworks');
